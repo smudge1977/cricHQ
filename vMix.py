fc621aea8aad12635d05f2b46bf44f3bcc6077dd
smudge1977 <smudge1977@hotmail.com>
2020-10-05 21:56:41 +0100

    Next to do the dictionary build function! XML fun!

--- vMix.py
+++ vMix.py
@@ -6,6 +6,7 @@ import logging, logging.config
 import yaml
 import xml
 import sys
+import datetime, time
 import socket
 
 class VMix:
@@ -13,38 +14,84 @@ class VMix:
         self.vMixIP = ip
         self._NameValues = {'TCSld460' : { 'current' : 'BAT NAME 1', 'pending' : 'BAT NAME 1', 'inputs' : ['c3998cb4-afbc-4411-b1b9-c3dc10f02d4d']}}
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._refresh()
+        #self._refresh()
     def _connect(self):
         try:
             self.s.connect((self.vMixIP, 8099))
-        except ConnectionRefusedError:
-            print(sys.exc_info()[0])
-            logger.error(f'VMix._connect : Error with _connect : ConnectionRefusedError {sys.exc_info()[0]}')
+            logger.info(f'{self.s.recv(1024)}')
+            self._execute('XML\r\n')    # whenever we connect the _execute will then call the xml processor 
+        except ConnectionRefusedError as e:
+            logger.error(f'VMix._connect : Error with _connect : ConnectionRefusedError {e}')
+            return False
         except:
-            logger.error(f'VMix._connect : Error with _connect : {sys.exc_info()[0]}')
+            logger.error(f'VMix._connect : Error with _connect : {str(sys.exc_info()[0])}')
             raise
         #logger.info(f'{self.s.recv(1024)}')
-    '''
-    except OSError as err:
-        print("OS error: {0}".format(err))
-    except ValueError:
-        print("Could not convert data to an integer.")
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        raise
-    '''
-    def _execute(self, command):
+    def _execute(self, command, retry=3):
+        for i in range(retry):
+            if self.__execute(command):
+                logger.debug(f'_execute retry count {i}')
+                break
+        
+    def __execute(self, command, _errCount=0):
         logger.info(f'VMix._execute : command : {command}')
+        #self.s.sendall(bytes(command,"utf-8"))
         try:
-            self.s.sendall(command)
-        except:
-            logger.debug(f'VMix._execute : Socket not open so reconnect...')
+            self.s.sendall(bytes(command,"utf-8"))
+        except OSError as e:
+            logger.debug(f'VMix._execute : Socket not open so reconnect... {e}')
             self._connect()
-        return True
+            #self._execute(command)
+            return False
+        if command.startswith('FUNCTION'):
+            response = self.s.recv(25)
+            response = response.decode('utf-8')
+            logger.debug(f'VMix._execute : response from {command[:-2]} : {response}')
+            if 'FUNCTION OK Completed' in response:
+                logger.info(f'VMix._execute : command : {command[:-2]} : OK')
+                return True
+            else:
+                logger.error(f'VMix._execute : command : {command[:-2]} : {response}')
+                return response
+        elif command.startswith('XML'):
+            done = False
+            new_msg = True
+            full_xml = ''
+            while not done:
+                try:
+                    data = self.s.recv(1024)
+                except ConnectionAbortedError:
+                    logger.error(f'vMixXML : Connection Aborted')
+                    xml = None
+                    break
+                xml = data.decode('utf-8')
+                if new_msg:
+                    # What is our length then?
+                    msglen = int(xml[xml.find('XML ') + 4 : xml.rfind('\r\n')])  # 4 is length of 'XML '
+                    new_msg = False
+                    logger.debug(f'vMixXML : msglen is {msglen}')
+                else:
+                    full_xml += xml
+                if len(full_xml) >= msglen:
+                    done = True
+                    logger.debug(f'VMix.__execute : length full_xml {len(full_xml)} and msglen {msglen}')
+                    break
+                logger.debug(f'vMixXML : length {len(full_xml)}')
+            logger.info(f'vMixXML : returning {full_xml}')
+            self._processXML(full_xml)
+            return full_xml
+        else:
+            response = self.s.recv(50)
+            response = response.decode('utf-8')
+            logger.error(f'VMix._execute : NOT SURE HOW TO HANDEL THIS COMMAND : {command[:-2]} : {response}')
+        return
 
-    def _refresh(self):
+    def _processXML(self, xml):
+        # *** now lets build our mega dict!
+        starttime = datetime.datetime.now()
+        logger.info(f'VMix._processXML start time : {starttime}')
 
-        self._execute('XML')
+        logger.info(f'VMix._processXML done in : {datetime.datetime.now() - starttime} : result is : {self._NameValues}')
         pass
     def setValue(self, name, value):
         try:
@@ -53,11 +100,11 @@ class VMix:
         except KeyError:
             logger.error(f'VMix.setValue : KeyError bad name passed : {name}')
             return False
-        
-        
+        self._execute('FUNCTION SetText Input=1&SelectedName=SCORETOTAL&Value=cc\r\n')
     def _command(self, command):
         response = ''
         return response
+
 # MAIN!!!
 if __name__ == "__main__":
     # https://stackoverflow.com/questions/38537905/set-logging-levels
