85cc6f9da99ade9f7cef698ea917c314b84772d2
smudge1977 <smudge1977@hotmail.com>
2020-10-06 21:26:33 +0100

    vMix module done - next make threaded

--- vMix.py
+++ vMix.py
@@ -4,7 +4,7 @@ This module provides the interface to vMix
 '''
 import logging, logging.config
 import yaml
-import xml
+from lxml import etree
 import sys
 import datetime, time
 import socket
@@ -14,6 +14,10 @@ class VMix:
         self.vMixIP = ip
         self._NameValues = {'TCSld460' : { 'current' : 'BAT NAME 1', 'pending' : 'BAT NAME 1', 'inputs' : ['c3998cb4-afbc-4411-b1b9-c3dc10f02d4d']}}
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.vMixXML = ''
+        self.vMixXMLtime = datetime.datetime.now()
+        self._desired = {}
+        self._actual = {}
         #self._refresh()
     def _connect(self):
         try:
@@ -28,6 +32,8 @@ class VMix:
             raise
         #logger.info(f'{self.s.recv(1024)}')
     def _execute(self, command, retry=3):
+        if command[:-2] is not '\r\n':
+            command += '\r\n'
         for i in range(retry):
             if self.__execute(command):
                 logger.debug(f'_execute retry count {i}')
@@ -83,24 +89,42 @@ class VMix:
         else:
             response = self.s.recv(50)
             response = response.decode('utf-8')
-            logger.error(f'VMix._execute : NOT SURE HOW TO HANDEL THIS COMMAND : {command[:-2]} : {response}')
+            logger.error(f'VMix._execute : NOT SURE HOW TO HANDEL THIS COMMAND : {command[:-2]} : Response : {response}')
         return
 
     def _processXML(self, xml):
         # *** now lets build our mega dict!
         starttime = datetime.datetime.now()
         logger.info(f'VMix._processXML start time : {starttime}')
-
+        self.vMixXML = xml
+        # Just going to build _disired and _actual on the fly by request instead
         logger.info(f'VMix._processXML done in : {datetime.datetime.now() - starttime} : result is : {self._NameValues}')
         pass
     def setValue(self, name, value):
+        self._desired[name] = value
+        logger.info(f'VMix.setValue : _desired = {self._desired}')
         try:
-            self._NameValues[name]['pending'] = value
-            logger.debug(f'VMix.setValue : self._NameValues[name] {self._NameValues[name]}')
-        except KeyError:
-            logger.error(f'VMix.setValue : KeyError bad name passed : {name}')
-            return False
-        self._execute('FUNCTION SetText Input=1&SelectedName=SCORETOTAL&Value=cc\r\n')
+            if self._actual[name] is not self._desired[name]:
+                logger.info(f'VMix.setValue need to update {name} from {self._actual[name]} to {self._desired[name]}')
+        except KeyError as error:
+            self._actual[name] = ''
+            logger.info(f'VMix.setValue need to update {name} from UNKOWN to {self._desired[name]}')
+        
+        #* Put this bit in its own thread to allow high speed data like timing to be push at vMix faster than it can be updated
+        starttime = datetime.datetime.now()
+        logger.info(f'VMix._processValues start time : {starttime}')
+        for k, v in self._desired.items():
+            if self._desired[k] is not self._actual[k]:
+                # Do find and update
+                if self.vMixXML == '':
+                    #* Improve to update old XML
+                    logger.info(f'VMIX.Update : no XML')
+                    self._execute('XML')
+                root = etree.fromstring(self.vMixXML)
+                for element in root.xpath(f"//text[@name='{k}']"):
+                    self._execute(f'FUNCTION SetText Input={element.getparent().get("key")}&SelectedName={k}&Value={v}')
+        logger.info(f'VMix._processValues done in : {datetime.datetime.now() - starttime}')
+
     def _command(self, command):
         response = ''
         return response
@@ -117,5 +141,10 @@ if __name__ == "__main__":
 
     vMix = VMix()
     vMix.setValue('TCSld460','bob')
-    print(vMix._NameValues)
+    vMix.setValue('TCSld490','fred')
+    vMix.setValue('TCSld460','tim')
+    vMix.setValue('NoThere','another')
+    vMix.setValue('BAT1','another')
+    
+    
     
